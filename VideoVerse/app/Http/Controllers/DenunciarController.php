c3dc375ea291fcd9e404995da2372fa19b60839b
Maria Rocha <marialuiza.aluno@unipampa.edu.br>
2023-11-14 01:49:26 -0300

    Criação da tela de denuncia + Controlador para Denúncia + Banco de Dados

---
+++ VideoVerse/app/Http/Controllers/DenunciarController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class DenunciaController extends Controller
+{
+    public function view()
+    {
+        // Retorne a view correspondente para o formulário de denúncia
+        return view('denuncia');
+    }
+
+    public function denunciar(Request $request)
+    {
+        // Obtenha os dados do formulário
+        $id_video = $request->input('id_video');
+        $outro = $request->input('outro');
+        $data = Carbon::now()->toDateString();
+        $hora = Carbon::now()->toTimeString();
+        $status = false; 
+
+        $request->validate([
+            'id_video' => 'required|numeric',
+            'outro' => 'nullable|string|max:255',
+        ]);
+
+        try {
+            // Insira os dados no banco de dados
+            $id_denuncia = DB::table('denuncias')->insertGetId([
+                'id_video' => $id_video,
+                'outro' => $outro,
+                'data' => $data,
+                'hora' => $hora,
+                'status' => $status,
+            ]);
+
+            return redirect()->route('pagina_de_denuncia')->with('success', 'Denúncia enviada com sucesso!');
+
+        } catch (\Exception $e) {
+            return redirect()->route('pagina_de_denuncia')->with('error', 'Erro ao processar denúncia: ' . $e->getMessage());
+        }
+    }
+}
---
+++ VideoVerse/resources/views/denunciar.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Denúnciar Vídeo</title>
+    <link rel="stylesheet" href="VideoVerse/resources/css/styles.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/criar_canal.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body style="background: #1A1818;">
+@section('content')
+@extends('layouts.upbar')
+@extends('layouts.sidebar')
+<div class="report-modal" id="reportModal">
+    <div class="modal-content">
+        <span class="close" onclick="closeModal()">&times;</span>
+        <h2>Denunciar Vídeo</h2>
+        <form action="#" method="post" id="reportForm">
+            <label>Motivo da Denúncia:</label><br>
+            <input type="radio" id="conteudo-sexual" name="reason" value="conteudo-sexual">
+            <label for="conteudo-sexual">Conteúdo Sexual</label><br>
+            <input type="radio" id="violento-repulsivo" name="reason" value="violento-repulsivo">
+            <label for="violento-repulsivo">Conteúdo Violento ou Repulsivo</label><br>
+            <input type="radio" id="odio-discurso" name="reason" value="odio-discurso">
+            <label for="odio-discurso">Discurso de Ódio</label><br>
+            <input type="radio" id="spam" name="reason" value="spam">
+            <label for="spam">Spam</label><br>
+            <input type="radio" id="engana-pessoas" name="reason" value="engana-pessoas">
+            <label for="engana-pessoas">Engana Pessoas</label><br>
+            <input type="radio" id="outro" name="reason" value="outro">
+            <label for="outro">Outro</label><br>
+
+            <label for="details">Detalhes (opcional):</label><br>
+            <textarea name="details" id="details" rows="4"></textarea><br>
+
+            <button type="submit">Enviar Denúncia</button>
+        </form>
+    </div>
+</div>
+@endsection
+</body>
+
+</html>
