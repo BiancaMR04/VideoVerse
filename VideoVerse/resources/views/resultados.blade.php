5c45b7c2fe2677e39266ba920116d5d59d7cf48a
KauaHenrico <108253678+KauaHenrico@users.noreply.github.com>
2023-11-18 23:26:42 -0300

    redirecionamento para video ao pesquisar

--- VideoVerse/resources/views/resultados.blade.php
+++ VideoVerse/resources/views/resultados.blade.php
@@ -88,39 +88,38 @@
 <div class="content">
 <!-- Exibição de vídeos -->
     <h1>Resultados da Pesquisa para "{{ $query }}"</h1>
+<!-- Exibição de canal -->
+<div class="content">
 
-        @forelse ($canais as $canal)
-            <div class="information">
-                <img src="{{ asset('uploads/' . $canal->imagem_perfil) }}" class="profile-image">
-                <div class="info">
-                    <h3>{{ $canal->nome }}</h3>
-                    <p>{{ $canal->descricao }}</p>
-                    <p>{{ $canal->inscritos }} inscritos</p>
-                </div>
-            </div>
-        @empty
-            <p>Nenhum canal encontrado.</p>
-        @endforelse
-    </div>
+    @forelse ($canais as $canal)
+    <a href="/canal/{{ $canal->id }}" class="information_canal">
+        <img src="{{ asset('uploads/' . $canal->imagem_perfil) }}" class="profile-image">
+        <div class="info">
+            <h3>{{ $canal->nome }}</h3>
+            <p>{{ $canal->descricao }}</p>
+            <p>{{ $canal->inscritos }} inscritos</p>
+        </div>
+    </a>
+    @empty
+        <p>Nenhum canal encontrado.</p>
+    @endforelse
 
     <!-- Seção de Vídeos -->
     <div>
-        @forelse ($videos as $video)
-        <div class="information">
-                <img src="{{ $video->caminho_imagem }}" alt="Thumbnail do Vídeo" class="video-thumbnail">
-                <div class="info">
-                    <h2>{{ $video->titulo }}</h2>
-                    <p>{{ $video->canal->nome }}</p>
-                    <p>{{ $video->visualizacao }} visualizações</p>
-                    <p>{{ $video->data }}</p>
-                </div>
-            </div>
-        @empty
-            <p>Nenhum vídeo encontrado.</p>
-        @endforelse
-    </div>
+    @forelse ($videos as $video)
+    <a href="{{ route('video.show', ['id' => $video->id]) }}">
+        <img src="{{ $video->caminho_imagem }}" alt="Thumbnail do Vídeo" class="video-thumbnail">
+        <div class="info">
+            <h2>{{ $video->titulo }}</h2>
+            <p>{{ $video->canal->nome }}</p>
+            <p>{{ $video->visualizacao }} visualizações</p>
+            <p>{{ $video->data }}</p>
+        </div>
+    </a>
+    @empty
+        <p>Nenhum vídeo encontrado.</p>
+    @endforelse
 </div>
-
 </body>
 @endsection
 </html>
